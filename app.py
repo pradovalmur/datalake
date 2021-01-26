0894cf01943391b1324af9abc16dfa8a7396f70f
valmur prado <pradovalmur@gmail.com>
2021-01-26 14:56:07 -0300

    Criando o Glue Catalog - bug da alteração do nome do bucket

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from aws_cdk import core
 
-from stacks.data_lake.stack import DatalakeStack
+from stacks.data_lake.stack import DataLakeStack
+from stacks.glue_catalog.stack import GlueCatalogStack
 
 app = core.App()
-data_lake = DatalakeStack(app)
+data_lake = DataLakeStack(app)
+glue_catalog = GlueCatalogStack(app, data_lake_bucket=data_lake.data_lake_raw_bucket)
 app.synth()
