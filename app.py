2792f6c4210399d9104bd42a67e7295e561bf016
valmur prado <pradovalmur@gmail.com>
2021-01-31 16:33:11 -0300

    adiciona kinesis e arruma glue catalog

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ from aws_cdk import core
 from stacks.data_lake.stack import DataLakeStack
 from stacks.glue_catalog.stack import GlueCatalogStack
 from stacks.athena.stack import AthenaStack
+from kineses.stack import KinesisStack
 
 app = core.App()
 data_lake = DataLakeStack(app)
 glue_catalog = GlueCatalogStack(app, data_lake_bucket=data_lake.data_lake_raw_bucket)
 athena = AthenaStack(app)
+kinesis = KinesisStack(app, data_lake_bucket=data_lake.data_lake_raw_bucket)
+
 app.synth()
